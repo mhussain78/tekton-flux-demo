0f430342c03563ea68d09cc87af703ee0e497f6e
mohamad <inf.mhussain@googlemail.com>
2023-11-28 16:36:25 +0100

    extended api

--- src/main/java/com/mhussain/tektonfluxdemo/CustomerController.java
+++ src/main/java/com/mhussain/tektonfluxdemo/CustomerController.java
@@ -1,20 +1,41 @@
 package com.mhussain.tektonfluxdemo;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
 @RestController
+@RequestMapping("/api/customers")
 @RequiredArgsConstructor
 public class CustomerController {
 
     private final CustomerRepository customerRepository;
 
-    @GetMapping("/api/customers")
+    @GetMapping
     public List<Customer> getAll() {
         return customerRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable("id") Long id) {
+        return ResponseEntity.of(customerRepository.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Customer customer) {
+        customerRepository.save(customer);
+        var location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(customer.getId())
+                .toUriString();
+        return ResponseEntity.status(CREATED).header(HttpHeaders.LOCATION, location).build();
     }
 
 }
